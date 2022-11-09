bee8f2e90dc66af87a812f469d0119d685ad7c26
taniaolivia <taniaolivia@MacBook-Pro-de-Tania.local>
2022-11-09 11:50:41 +0100

    add axios aysnc await

--- src/providers/textApiProvider.js
+++ src/providers/textApiProvider.js
@@ -2,15 +2,7 @@ const axios = require("axios");
 
 const baseUrl = "https://loripsum.net/api";
 
-exports.getRandomText = () => {
-    return Promise((resolve, reject) => {
-        axios.get(baseUrl + "/plaintext", {responseType: "text"})
-        .then((response) => {
-            resolve(response.data);
-        })
-        .catch(error => {
-            console.log(error);
-            reject(false);
-        })
-    })
+exports.getRandomText = async () => {
+    let response = await axios.get(baseUrl + "/plaintext", {responseType: "text"});
+    return response.data;
 }
